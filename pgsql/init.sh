9cc563103c49842f86575bf0ea84de5aa85e79a2
Vladislav Komissarov <mastyf@gmail.com>
2022-04-27 15:41:44 +0300

    defihelper_adapters database

--- pgsql/init.sh
+++ pgsql/init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-echo "SELECT 'CREATE DATABASE defihelper_backend' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'defihelper-backend')\gexec" | psql -U development
-echo "SELECT 'CREATE DATABASE defihelper_scanner' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'defihelper-scanner')\gexec" | psql -U development
+echo "SELECT 'CREATE DATABASE defihelper_backend' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'defihelper_backend')\gexec" | psql -U development
+echo "SELECT 'CREATE DATABASE defihelper_scanner' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'defihelper_scanner')\gexec" | psql -U development
+echo "SELECT 'CREATE DATABASE defihelper_adapters' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'defihelper_adapters')\gexec" | psql -U development

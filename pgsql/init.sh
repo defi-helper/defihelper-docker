8e8bbf3b7d56a62ab06d6ed503cb54e6bb018200
Vladislav Komissarov <mastyf@gmail.com>
2022-09-19 17:16:01 +0300

    ranking database

--- pgsql/init.sh
+++ pgsql/init.sh
@@ -3,4 +3,7 @@
 echo "SELECT 'CREATE DATABASE defihelper_backend' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'defihelper_backend')\gexec" | psql -U development
 echo "SELECT 'CREATE DATABASE defihelper_scanner' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'defihelper_scanner')\gexec" | psql -U development
 echo "SELECT 'CREATE DATABASE defihelper_adapters' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'defihelper_adapters')\gexec" | psql -U development
+echo "SELECT 'CREATE DATABASE defihelper_ranking' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'defihelper_ranking')\gexec" | psql -U development
+
 psql -U development -d defihelper_adapters < /db.backup/adapters.sql
+psql -U development -d defihelper_ranking < /db.backup/ranking.sql
